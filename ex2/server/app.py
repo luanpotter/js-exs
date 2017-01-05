3c5d8a4f9cf0fae0571bc3032e1419db3795aaf1
Luan Nico <luanpotter27@gmail.com>
2017-01-05 14:10:20 -0200

    Adding like action to post

--- ex2/server/app.py
+++ ex2/server/app.py
@@ -1,9 +1,12 @@
 # app.py
 
 import peewee
+import json
+from playhouse.shortcuts import model_to_dict
 from ray_peewee.all import PeeweeModel
 from ray.wsgi.wsgi import application
 from ray.endpoint import endpoint
+from ray.actions import Action, action
 
 database = peewee.SqliteDatabase('example.db')
 
@@ -14,7 +17,17 @@ class DBModel(PeeweeModel):
 @endpoint('/post')
 class Post(DBModel):
     title = peewee.CharField()
-    text = peewee.CharField()
+    likes = peewee.IntegerField(default=0)
+
+class PostActions(Action):
+    __model__ = Post
+
+    @action('/<id>/like')
+    def like(self, post_id, params):
+        post = Post.select().where(Post.id == post_id)[0]
+        post.likes = post.likes + 1
+        post.update()
+        return model_to_dict(post)
 
 if __name__ == '__main__':
     database.connect()
